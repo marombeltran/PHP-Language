d16277934ba31e6ea65898a83456c3a20a98b17b
Marom Beltran <mrmbeltran@proton.me>
2023-07-22 12:11:01 -0600

    First commit

---
+++ constants.php
@@ -0,0 +1,36 @@
+<?php
+/* Every single expression in PHP has one of the following built-in types depending on its value:
+ * null
+ * bool [true, false, TRUE, FALSE]
+ * int
+ * float (floating-point number)
+ * string
+ * array
+ * object
+ * callable
+ * resource
+ */
+
+/* The null type is PHP's unit type, i.e. it has only one value: null.
+ */
+NULL;
+$a = null;
+
+'';
+'\n';
+"\n";
+
+
+/* Integer Constansts */
+
+3; // decimal number
+0o234; // octal number
+0x1B; // hexadecimal number
+0b11111110; // binary number
+
+/* Decimal Constants */
+3_30; // decimal number
+3.30;
+1.2e3;
+7E-10;
+
