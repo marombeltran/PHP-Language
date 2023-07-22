5d272d4c66be85d01e4c8a07db8e4cd68f038b33
Marom Beltran <mrmbeltran@proton.me>
2023-07-22 15:18:19 -0600

    Identifiers

--- day-001.php
+++ day-001.php
@@ -13,16 +13,27 @@
  * - Features
  */
 
+/* 1.1 Identifiers
+ * --------------------------------------------------------------------------------------------
+ * Variable names follow the same rules as other labels in PHP. A valid variable name starts with 
+ * a letter or underscore, followed by any number of letters, numbers, or underscores. 
+ * As a regular expression, it would be expressed thus: ^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$
+ */
+
+/* Variables in PHP are represented by a dollar sign followed by the name of the variable. 
+ * The variable name is case-sensitive.
+ */
+
 /* Every single expression in PHP has one of the following built-in types depending on its value:
- * null
- * bool [true, false, TRUE, FALSE]
- * int
- * float (floating-point number)
- * string
- * array
- * object
- * callable
- * resource
+ * - null
+ * - bool [true, false, TRUE, FALSE]
+ * - int
+ * - float (floating-point number)
+ * - string
+ * - array
+ * - object
+ * - callable
+ * - resource
  */
 
 /* The null type is PHP's unit type, i.e. it has only one value: null.
@@ -34,16 +45,16 @@ $a = null;
 '\n';
 "\n";
 
+/**
+ * Integer Constansts */
+3; // decimal
+0o234; // octal
+0x1B; // hexadecimal
+0b11111110; // binary
 
-/* Integer Constansts */
-
-3; // decimal number
-0o234; // octal number
-0x1B; // hexadecimal number
-0b11111110; // binary number
-
-/* Decimal Constants */
-3_30; // decimal number
+/**
+ * Decimal Constants */
+3_30;
 3.30;
 1.2e3;
 7E-10;
