5820af5dbeaca90dae9b003e8c5ce2081bf4296f
Marom Beltran <mrmbeltran@proton.me>
2023-07-22 13:27:14 -0600

    Change - day 001

--- day-001.php
+++ day-001.php
@@ -1,4 +1,18 @@
 <?php
+
+/**
+ * PHP, which stands for "PHP: Hypertext Preprocessor" is a widely-used Open Source general-purpose 
+ * scripting language that is especially suited for web development and can be embedded into HTML. 
+ * Its syntax draws upon C, Java, and Perl, and is easy to learn. 
+ *
+ * - The main goal of the language is to allow web developers to write dynamically generated web pages 
+ *   quickly, but you can do much more with PHP.
+ *
+ * - Language reference ******
+ * - Function referece
+ * - Features
+ */
+
 /* Every single expression in PHP has one of the following built-in types depending on its value:
  * null
  * bool [true, false, TRUE, FALSE]
