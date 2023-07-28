1efda4d679f9b88c10ef4dee80de2f4ae0581426
Marom Beltran <mrmbeltran@proton.me>
2023-07-27 20:35:16 -0600

    Change - structure

--- expressions.php
+++ expressions.php
@@ -1,15 +1,40 @@
 <?php
-/* Variables in PHP are represented by a dollar sign followed by the name of the variable. 
- * The variable name is case-sensitive.
- *
- * -------------------------------------------------------------------------------------------------------------------------
+/* By default, variables are always assigned by value. That is to say, when you assign an expression 
+ * to a variable, the entire value of the original expression is copied into the destination variable. 
+ * This means, for instance, that after assigning one variable's value to another, changing one of 
+ * those variables will have no effect on the other.
+ */
+
+/* The null type is PHP's unit type, i.e. it has only one value: null.
+ */
+NULL;
+$a = null;
+
+'';
+'\n';
+"\n";
+
+/**
+ * Integer Constansts */
+3; // decimal
+0o234; // octal
+0x1B; // hexadecimal
+0b11111110; // binary
+
+/**
+ * Decimal Constants */
+3_30;
+3.30;
+1.2e3;
+7E-10;
+
+/* -------------------------------------------------------------------------------------------------------------------------
  * Expressions
  * -------------------------------------------------------------------------------------------------------------------------
  * Expressions are the most important building blocks of PHP. In PHP, almost anything you write is 
  * an expression. The simplest yet most accurate way to define an expression is "anything that has a value".
- */
 
-/* Every single expression in PHP has one of the following built-in types depending on its value:
+ * Every single expression in PHP has one of the following built-in types depending on its value:
  * - null
  * - bool [true, false, TRUE, FALSE]
  * - int
--- variables_CONST.php
+++ variables_CONST.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * By default, variables are always assigned by value. That is to say, when you assign an expression 
- * to a variable, the entire value of the original expression is copied into the destination variable. 
- * This means, for instance, that after assigning one variable's value to another, changing one of 
- * those variables will have no effect on the other.
+/* Variables in PHP are represented by a dollar sign followed by the name of the variable. 
+ * The variable name is case-sensitive.
  */
+
