d950b82c01b1c31c8ebec16db7b82203483e60c0
Marom Beltran <mrmbeltran@proton.me>
2023-07-27 20:43:15 -0600

    Change - structure

--- expressions.php
+++ expressions.php
@@ -1,10 +1,4 @@
 <?php
-/* By default, variables are always assigned by value. That is to say, when you assign an expression 
- * to a variable, the entire value of the original expression is copied into the destination variable. 
- * This means, for instance, that after assigning one variable's value to another, changing one of 
- * those variables will have no effect on the other.
- */
-
 /* The null type is PHP's unit type, i.e. it has only one value: null.
  */
 NULL;
