ff8d2f837dc1c043fce4d52c12807e06cfda85cb
Marom Beltran <mrmbeltran@proton.me>
2023-07-27 21:13:39 -0600

    Change - environment

--- 001 - environment.php
+++ 001 - environment.php
@@ -1,6 +1,7 @@
 <?php
-/* Identifiers
- * --------------------------------------------------------------------------------------------
+/* -------------------------------------------------------------------------------------------------------------------------
+ * Identifiers
+ * -------------------------------------------------------------------------------------------------------------------------
  * Identifiers are sequences of characters used for naming variables, functions, classes, constants 
  * and another elements of code.
  */
@@ -26,7 +27,9 @@
  * - constants (not class constants)
  * - variables defined outside of functions/methods (nested functions are allowed)
  *
+/* -------------------------------------------------------------------------------------------------------------------------
  * Some Rules
+ * -------------------------------------------------------------------------------------------------------------------------
  * - Function names use underscores between words, while class names use both the camelCase and PascalCase rules.
  * - PHP will prefix any global symbols of an extension with the name of the extension. curl_close(), mysql_query()
  * - Iterators and Exceptions are however simply postfixed with "Iterator" and "Exception." ArrayIterator, LogicException.
@@ -34,7 +37,9 @@
  *   symbols starting with __ in PHP unless you want to use documented magical functionality.
  *   __get(), __autoload().
  *
+/* -------------------------------------------------------------------------------------------------------------------------
  * Tips
+ * -------------------------------------------------------------------------------------------------------------------------
  * In order to write future-proof code, it is recommended that you don't place many variables, functions 
  * or classes in the global namespace. This will prevent naming conflicts with 3rd party code as well as 
  * possible future additions to the language.
@@ -51,3 +56,4 @@
  *
  *   \MyProject\my_function();
  */
+
